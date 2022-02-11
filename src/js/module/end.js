ff8b264da7a6c7cd1d8e40ceff15ce2aa586c6e8
ChincharovPC <chincharovpc@yandex.ru>
2022-02-12 02:45:37 +0300

    copy code

--- src/js/module/end.js
+++ src/js/module/end.js
@@ -1,7 +1,10 @@
 const buttonEnd = document.querySelector(".end .button__end")
 const present = document.querySelector(".end .present")
 const closeButton = document.querySelector(".end .present .close__button")
-
+const copy = document.querySelector(".end .present .copy")
+const code = document.querySelector(".end .present .name")
 
 buttonEnd.addEventListener('click', () => present.classList.remove('hidden'))
 closeButton.addEventListener('click', () => present.classList.add('hidden'))
+
+copy.addEventListener('click', () => navigator.clipboard.writeText(code.textContent))
