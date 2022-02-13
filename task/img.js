93d913967b60641a891dc622af421ef7046f7c09
ChincharovPC <chincharovpc@yandex.ru>
2022-02-13 16:23:40 +0300

    revert salmon

--- task/img.js
+++ task/img.js
@@ -23,7 +23,7 @@ const img = () => {
       }))
     }))
     .pipe(newer(path.img.dest))
-    //.pipe(webp())
+    .pipe(webp())
     .pipe(dest(path.img.dest))
     .pipe(src(path.img.src))
     .pipe(newer(path.img.dest))
--- task/scss.js
+++ task/scss.js
@@ -29,7 +29,7 @@ const scss = () => {
     }))
     .pipe(sassGlob())
     .pipe(sass())
-    //.pipe(webpCss())
+    .pipe(webpCss())
     .pipe(autoprefixer())
     .pipe(shorthand())
     .pipe(groupMedia())
